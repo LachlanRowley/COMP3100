675554173c65d1563d1164a221c824be5e5cdabc
Lachlan Rowley <lachlan.rowley@students.mq.edu.au>
2022-03-17 12:25:41 +1100

    DS-sim client

--- client.java
+++ client.java
@@ -1,20 +1,28 @@
-import java.net.*;  
 import java.io.*;  
-class MyClient{  
-    public static void main(String args[])throws Exception{  
-        Socket s=new Socket("localhost",3333);  
-        DataInputStream din=new DataInputStream(s.getInputStream());  
-        DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
-        
-        String str="",str2="";  
-        while(!str.equals("stop")){  
-            str=br.readLine();  
-            dout.writeUTF(str);  
-            dout.flush();  
-            str2=din.readUTF();  
-            System.out.println("Server says: "+str2);  
-        }         
-        dout.close();  
-        s.close();  
-}}  
+import java.net.*;  
+public class MyClient {  
+    public static void main(String[] args) {  
+    try{      
+	    Socket s=new Socket("localhost",50000);  
+	    DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+	    //DataInputSteam din = new DataInputSteam(s.getInputStream());
+	    BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+	    String command = "";  
+	    dout.write(("HELO\n").getBytes());  
+	    command = br.readLine();
+	    System.out.println(command);
+	    String username = System.getProperty("user.name");
+	    dout.write(("AUTH " + username +  "\n").getBytes());
+   	    command = br.readLine();
+	    System.out.println(command);
+	    dout.write(("REDY\n").getBytes());
+	    command = br.readLine();
+	    System.out.println(command);
+   	    dout.write(("QUIT\n").getBytes());
+	    dout.flush();
+	    dout.close();
+	    br.close();  
+	    s.close();  
+    	}catch(Exception e){System.out.println(e);}
+    }
+}  
